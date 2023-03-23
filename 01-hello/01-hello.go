194db0f1a1053084e7596dda7610be37d81bb782
iower <iower@tutanota.com>
2023-03-24 00:41:36 +0500

    Vars

--- 01-hello/01-hello.go
+++ 01-hello/01-hello.go
@@ -1,6 +1,11 @@
 package main
+
 import "fmt"
 
 func main() {
 	fmt.Println("Hello Go!")
+	fmt.Println("Hello Go2!")
+	fmt.Println("Hello Go3!")
+
+	varst()
 }
---
+++ 02-vars/02-vars.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string
+	var a, b, c int
+
+	hello = "Hello world"
+	a = 1
+	b = 2
+	c = 3
+
+	fmt.Println(hello, a, b, c)
+}
