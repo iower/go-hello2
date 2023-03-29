974323b3875e810ed7c8d062cdc73887060fee0b
iower <iower@tutanota.com>
2023-03-29 23:38:31 +0500

    Cycles 2

--- 10-cycles/10-cycles.go
+++ 10-cycles/10-cycles.go
@@ -31,5 +31,37 @@ func main() {
 		fmt.Println()
 	}
 
-	// ...
+	// range
+	users := [3]string{"Tom", "Alice", "Kate"}
+	for index, value := range users {
+		fmt.Println(index, value)
+	}
+	for _, value := range users {
+		fmt.Println(value)
+	}
+	for i := 0; i < len(users); i++ {
+		fmt.Println(i, users[i])
+	}
+
+	// break, continue
+	var numbers = [10]int{1, -2, 3, -4, 5, -6, -7, 8, -9, 10}
+	var sum = 0
+	for _, value := range numbers {
+		if value < 0 {
+			continue
+		}
+		sum += value
+	}
+	fmt.Println("Sum:", sum)
+
+	var numbers2 = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var sum2 = 0
+
+	for _, value := range numbers2 {
+		if value > 4 {
+			break
+		}
+		sum2 += value
+	}
+	fmt.Println("Sum2:", sum2)
 }
