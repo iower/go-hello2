9cf5d21a7025c138f5f1953a0ab29d0709888acc
iower <iower@tutanota.com>
2023-04-06 14:02:14 +0500

    Func types 3

--- 12-func-types/12-func-types.go
+++ 12-func-types/12-func-types.go
@@ -17,10 +17,27 @@ func main() {
 	//
 	action(10, 15, add)
 	action(5, 6, multiply)
+
+	//
+	slice := []int{-2, 4, 3, -1, 7, -4, 23}
+	sumOfEvens := sum(slice, isEven)
+	fmt.Println(sumOfEvens)
+	sumOfPositives := sum(slice, isPositive)
+	fmt.Println(sumOfPositives)
+
+	// return function
+	f = selectFn(1)
+	fmt.Println(f(3, 4))
+	f = selectFn(3)
+	fmt.Println(f(3, 4))
 }
 
 func add(x, y int) int {
 	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
 }
 
 func multiply(x, y int) int {
@@ -34,4 +51,32 @@ func display(message string) {
 func action(n1, n2 int, operation func(int, int) int) {
 	result := operation(n1, n2)
 	fmt.Println(result)
+}
+
+func isEven(n int) bool {
+	return n%2 == 0
+}
+
+func isPositive(n int) bool {
+	return n > 0
+}
+
+func sum(numbers []int, criteria func(int) bool) int {
+	result := 0
+	for _, val := range numbers {
+		if criteria(val) {
+			result += val
+		}
+	}
+	return result
+}
+
+func selectFn(n int) func(int, int) int {
+	if n == 1 {
+		return add
+	} else if n == 2 {
+		return subtract
+	} else {
+		return multiply
+	}
 }
