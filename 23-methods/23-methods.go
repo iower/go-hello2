69355b8a30a0ff8414d355aa29b28bf662cdd2dd
iower <iower@tutanota.com>
2023-04-19 22:45:29 +0500

    Methods 2

--- 23-methods/23-methods.go
+++ 23-methods/23-methods.go
@@ -10,7 +10,24 @@ func (l library) print() {
 	}
 }
 
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) print() {
+	fmt.Println("Name: ", p.name)
+	fmt.Println("Age: ", p.age)
+}
+func (p person) eat(meal string) {
+	fmt.Println(p.name, "eats", meal)
+}
+
 func main() {
 	myLibrary := library{"book1", "book2", "book3"}
 	myLibrary.print()
+
+	tom := person{name: "Tom", age: 24}
+	tom.print()
+	tom.eat("Soup")
 }
