7faa78c2ed8e208131927959f541d875647ea9cc
iower <iower@tutanota.com>
2023-04-24 22:54:57 +0500

    External module

--- 25-packages/factorial/main.go
+++ 25-packages/factorial/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"rsc.io/quote"
+
 	"factorial/computation"
 )
 
 func main() {
 	fmt.Println(computation.Factorial(4))
 	fmt.Println(computation.Factorial(5))
+
+	fmt.Printf(quote.Hello())
 }
