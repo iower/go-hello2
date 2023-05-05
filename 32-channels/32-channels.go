e6778625272caa6c78151382c370fb3f08483212
iower <iower@tutanota.com>
2023-05-05 22:41:13 +0500

    Channels 2

--- 32-channels/32-channels.go
+++ 32-channels/32-channels.go
@@ -1,12 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	var intCh chan int
+	var intCh chan int = make(chan int)
 	fmt.Println(intCh)
 
-	intCh <- 5
-	val := <-intCh
-	fmt.Println(val)
+	go func() {
+		fmt.Println("Go routine starts")
+		intCh <- 5
+		intCh <- 6
+		fmt.Println("Go routine ends")
+	}()
+
+	fmt.Println("Wait...")
+	fmt.Println(<-intCh)
+	fmt.Println("The End")
+
+	//
+	fmt.Println()
+
+	myCh := make(chan int)
+	go factorial(5, myCh)
+	fmt.Println("Wait2...")
+	time.Sleep(2 * time.Second)
+	fmt.Println(<-myCh)
+	fmt.Println("The End 2")
+}
+
+func factorial(n int, ch chan int) {
+	fmt.Println("Go factorial...")
+	res := 1
+	for i := 1; i <= n; i++ {
+		res *= i
+	}
+	fmt.Println(n, "-", res)
+
+	fmt.Println("Send...")
+	ch <- res
+	fmt.Println("Go factorial ends")
 }
