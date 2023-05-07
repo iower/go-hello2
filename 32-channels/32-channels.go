c0ba3f9f9e899171f3e20c48f4c7f687441b3850
iower <iower@tutanota.com>
2023-05-07 20:58:49 +0500

    Channels 3

--- 32-channels/32-channels.go
+++ 32-channels/32-channels.go
@@ -29,9 +29,26 @@ func main() {
 	time.Sleep(2 * time.Second)
 	fmt.Println(<-myCh)
 	fmt.Println("The End 2")
+
+	// buffered
+	myChBuffered := make(chan int, 3)
+	fmt.Println("cap", cap(myChBuffered), "len", len(myChBuffered))
+	myChBuffered <- 1
+	myChBuffered <- 2
+	myChBuffered <- 3
+	// myChBuffered <- 4 // blocking
+	fmt.Println(<-myChBuffered)
+	fmt.Println(<-myChBuffered)
+	fmt.Println(<-myChBuffered)
+	fmt.Scan()
+
+	//
+	fmt.Println("Start")
+	fmt.Println(<-createChan(5))
+	fmt.Println("End")
 }
 
-func factorial(n int, ch chan int) {
+func factorial(n int, ch chan<- int) {
 	fmt.Println("Go factorial...")
 	res := 1
 	for i := 1; i <= n; i++ {
@@ -42,4 +59,12 @@ func factorial(n int, ch chan int) {
 	fmt.Println("Send...")
 	ch <- res
 	fmt.Println("Go factorial ends")
+}
+
+func createChan(n int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		ch <- n
+	}()
+	return ch
 }
