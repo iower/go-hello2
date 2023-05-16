8c6393ac269733811b3e5a3b3d87ce6592de0571
iower <iower@tutanota.com>
2023-05-16 16:54:43 +0500

    Files 2

--- 39-files/39-files.go
+++ 39-files/39-files.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -33,4 +34,28 @@ func main() {
 	fmt.Println(file4.Name(), file4, err4)
 	exitOnErr(err4)
 	defer file4.Close()
+
+	str := "Hi, world!\n"
+	file4.WriteString(str)
+
+	bytes := []byte("Hi, byte world!\n")
+	file4.Write(bytes)
+
+	fmt.Println("Done!")
+
+	// read
+	file5, err5 := os.Open("hi2.txt")
+	fmt.Println(file5.Name(), file5, err5)
+	exitOnErr(err5)
+	defer file5.Close()
+
+	data := make([]byte, 64)
+	for {
+		n, err := file5.Read(data)
+		fmt.Println(n, err)
+		if err == io.EOF {
+			break
+		}
+		fmt.Print(string(data[:n]))
+	}
 }
